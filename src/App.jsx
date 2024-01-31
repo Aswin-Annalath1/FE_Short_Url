0632679d59efa9db100d9cbe055086bed17f4a76
Aswin-Annalath1 <aswinannalath2001@gmail.com>
2024-01-31 23:55:30 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from './components/Signup';
 import Forgot from './components/Forgot';
-import NewPassword from './components/Newpassword';
+import NewPassword from './components/NewPassword';
 import Dashboard from './components/Dashboard';
 import GetShortURL from './innercomponents/GetShortURL';
 
